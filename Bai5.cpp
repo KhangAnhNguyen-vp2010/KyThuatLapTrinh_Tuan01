3a4e2148e5ca88b433b701c0b449aa373d551355
Anh Khang <taicuoi664@gmail.com>
2024-07-11 15:41:19 +0700

    Add Bai So 5

---
+++ Bai5.cpp
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#include <conio.h>
+//
+//int main()
+//{
+//	int a,b,c;
+//	printf("Nhap a:");
+//	scanf("%d", &a);
+//	printf("Nhap b:");
+//	scanf("%d", &b);
+//	printf("Nhap c:");
+//	scanf("%d", &c);
+//	printf("Ket qua:%d", c/(a-b));
+//	getch();
+//	return 0;
+//}
